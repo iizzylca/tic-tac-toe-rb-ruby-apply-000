ca9ce48012d79a40ff226a4f7df5677a86a4f160
Israel Canessa <israellcanessa@gmail.com>
2020-05-14 07:59:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ def move(board, index, player)
 end
 
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+  board[index]== "X" || board[index] == "O"
 end
 
 def valid_move?(board, index)
